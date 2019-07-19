1c9482bca8ba27601e9ea3c5ca2d0dbd6fa2aae8
sumit <sumit@computronicsonline.com>
2019-07-19 12:54:43 +0530

    Update index.php
    remove unwanted lines

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 function getLongestCommonPrefix($array) {
 	$prefix = '';
 	if(!empty($array)) {
@@ -20,24 +19,6 @@ function getLongestCommonPrefix($array) {
 	}
 	return $prefix;
 }
-
 $array = array('zzzzzzzzzzzzzzz','zzzzzzzzzzzzz','zz');
 echo 'prefix = '.getLongestCommonPrefix($array);
-/* $lengths_array = array_map('strlen', $array);
-	$prefix_maxsize = min($lengths_array);
-	for($i=1;$i<=$prefix_maxsize;$i++) {
-		for($j=0;$j<sizeof($array);$j++) {
-			if($j>0) {
-				if(substr($array[$j],0,$i) === substr($array[$j-1],0,$i)) {
-					echo 'matched == '.$prefix = substr($array[$j],0,$i);
-				} else {
-					echo 'not matched == '.$prefix = substr($array[$j],0,$i-1);
-					break 2;
-				}
-			}
-		}
-	}
- */
-//echo getLongestCommonPrefix($array);
-
 ?>
